1936d8cf9b5d910d97ae8c49e5d087c6678bafc4
matt@pallissard.net <matt@pallissard.net>
2021-02-22 17:32:20 -0800

    wip

--- common_problems/in_memory_filesystem/in_memory_filesystem.ml
+++ common_problems/in_memory_filesystem/in_memory_filesystem.ml
@@ -1,51 +1,59 @@
-type content  = S of string | Int of int | Bytes of bytes | Dir
-type erros  = Use_mkdir_for_dirs | Exists_dir | Exists_file
+type content = S of string | Int of int | Bytes of bytes | Dir
+
+type errors = Use_mkdir_for_dirs | Exists_dir | Exists_file
+
 module FS = struct
-  module Tf = Map.Make(String)
+  module Tf = Map.Make (String)
 
   let empty = Tf.empty
+
   let sregxp = Str.regexp "/"
+
   let splitpath = Str.split sregxp
+
   let stat = Tf.mem
+
   let set path contents tree = Tf.add path contents tree
+
   let get path x = Tf.find path x
 
   let mkdir tree path =
     let rec aux tree = function
       | [] -> Error "exists"
-      | h :: t -> (match stat h tree with
-       | true -> aux tree t
-       | false -> create tree t)
+      | h :: t -> (
+        match stat h tree with
+        | true -> aux tree t
+        | false -> create tree t )
     and create tree = function
       | [] -> Ok tree
       | h :: t -> create (set h Dir tree) t
     in
     aux tree path
 
-  let write tree path contents = match contents with
+  let write tree path contents =
+    match contents with
     | Dir -> Error Use_mkdir_for_dirs
     | _ ->
-    let rec aux root = function
-      | [] -> Exists_file
-      | h :: t ->
-         let path = root ^ "/" ^ h in
-         match get path tree with
-          | true -> aux path t
-        |(match overwrite with false -> match t with
-
-    in
-    aux
-
-
-
+        let rec aux root = function
+          | [] -> Error Exists_file
+          | h :: t -> (
+              let path = root ^ "/" ^ h in
+              match get path tree with
+              | true -> aux path t
+              | false -> (
+                match t with
+                | [] -> Ok (set root contents tree)
+                | _ -> aux path t ) )
+        in
+        aux "/" (splitpath path)
 end
 
-let display i j =
-  Printf.printf i j;
+let display i j = Printf.printf i j
 
 exception Can't_display_type
+
 let () =
   match FS.set "foo" (S "stringfile") FS.empty |> FS.get "foo" with
-    | S i -> display "%s" i;
-    | Int i -> display "%d" i;
-    | Dir | Bytes _ -> raise Can't_display_type
+  | S i -> display "%s" i
+  | Int i -> display "%d" i
+  | Dir | Bytes _ -> raise Can't_display_type
