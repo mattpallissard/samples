323038f2482a50f7040361c0cf14ada422e46951
Matt Pallissard <matt@pallissard.net>
2021-03-11 20:45:21 -0800

    wip

--- common_problems/hackerrank/dna_health/health.ml
+++ common_problems/hackerrank/dna_health/health.ml
@@ -1,4 +1,4 @@
-type comparison = Lt | Gt | Eq
+let inc i = 1 + i
 
 module type SET = sig
   type t
@@ -27,37 +27,15 @@ module Trie (S : SET) = struct
 
   type data = S.score
 
+  type depth = int
+
   type 'a trie =
     | H of 'a children
-    | B of key * 'a children
-    | R of key * 'a children option * data
+    | B of key * depth * 'a children
+    | R of key * depth * 'a children option * data
     | Nil
 
   and 'a children = 'a trie T.t
-
-  (*https://www.lri.fr/~filliatr/publis/enum2.pdf *)
-
-  let get_fail c m =
-    let rec aux = function
-      | R (_, None, _) | Nil | H _ -> None
-      | (B (c', i) | R (c', Some i, _)) as j -> (
-          if S.eq c c' then Some j
-          else
-            try T.find c m
-            with Not_found -> (
-              match aux_seq (T.to_seq m |> List.of_seq) with
-              | Some i -> i
-              | None -> aux j ) )
-    and aux_seq = function
-      | [] -> None
-      | h :: t -> (
-        match h with
-        | B (_, i) | R (_, Some i, _) -> (
-          match aux h with
-          | Some i -> i
-          | None -> aux_seq t ) )
-    in
-    aux m
 
   let empty = Nil
 
@@ -69,58 +47,119 @@ module Trie (S : SET) = struct
     | [h] -> (
       match m with
       | H _ -> raise Not_found
-      | R (c, _, d) -> if S.eq h c then d else raise Not_found
-      | B (_, _) -> raise Not_found
+      | R (c, _, _, d) -> if S.eq h c then d else raise Not_found
+      | B (_, _, _) -> raise Not_found
       | Nil -> raise Not_found )
     | h :: t :: r -> (
       match m with
       | H m -> T.find h m |> get (t :: r)
-      | R (c, Some m, _) | B (c, m) ->
+      | R (c, _, Some m, _) | B (c, _, m) ->
           if S.eq h c then T.find t m |> get (t :: r) else raise Not_found
-      | R (_, None, _) -> raise Not_found
+      | R (_, _, None, _) -> raise Not_found
       | Nil -> raise Not_found )
+
+  let get_fail_path c m =
+    (* this strategy is pretty rough *) 
+    let rec aux acc m =
+      let rec run = function
+        | [] -> []
+        | (_, v) :: t -> aux [] v @ run t
+      in
+      let s i = T.to_seq i |> List.of_seq |> run in
+      match m with
+      | R (c', _, None, _) -> if S.eq c c' then m :: acc else acc
+      | Nil -> acc
+      | H i -> s i
+      | R (c', _, Some j, _) | B (c', _, j) ->
+          let acc' = if S.eq c c' then m :: acc else acc in
+          acc' @ s j
+    in
+    aux [] m
+
+  let rec display = function
+    | [] -> ()
+    | h :: t -> (
+      match h with
+      | Nil | H _ -> display t
+      | R (c, d, _, _) | B (c, d, _) ->
+          S.print c ; Printf.printf " %d\n" d ; display t )
 
   exception Neverhere
 
   let insert char_list data m =
-    let rec aux m = function
+    let rec aux depth m = function
       | [] -> raise Neverhere
-      | [h] -> R (h, None, data)
+      | [h] -> R (h, depth, None, data)
       | h :: t :: r -> (
         match m with
         | H m -> (
           match T.find_opt h m with
-          | Some i -> H (T.add h (aux i (t :: r)) m)
-          | None -> H (T.add h (aux Nil (t :: r)) m) )
-        | Nil -> B (h, T.add t (aux Nil (t :: r)) T.empty)
-        | B (c, m) -> (
+          | Some i -> H (T.add h (aux (inc depth) i (t :: r)) m)
+          | None -> H (T.add h (aux (inc depth) Nil (t :: r)) m) )
+        | Nil -> B (h, depth, T.add t (aux (inc depth) Nil (t :: r)) T.empty)
+        | B (c, depth, m) -> (
             if S.eq c h then
               match T.find_opt t m with
-              | Some i -> B (c, T.add t (aux i (t :: r)) m)
-              | None -> B (c, T.add t (aux Nil (t :: r)) m)
+              | Some i -> B (c, depth, T.add t (aux (inc depth) i (t :: r)) m)
+              | None -> B (c, depth, T.add t (aux (inc depth) Nil (t :: r)) m)
             else
               match T.find_opt h m with
-              | Some i -> aux i (t :: r)
-              | None -> aux (B (h, T.empty)) (t :: r) )
-        | R (c, m, d) -> (
+              | Some i -> aux (inc depth) i (t :: r)
+              | None -> aux (inc depth) (B (h, depth, T.empty)) (t :: r) )
+        | R (c, depth, m, d) -> (
           match m with
           | Some m -> (
               if S.eq c h then
                 match T.find_opt t m with
-                | Some i -> R (c, Some (T.add t (aux i r) m), d)
+                | Some i ->
+                    R (c, depth, Some (T.add t (aux (inc depth) i r) m), d)
                 | None ->
-                    R (c, Some (T.add t (aux (B (h, T.empty)) (t :: r)) m), d)
+                    R
+                      ( c
+                      , depth
+                      , Some
+                          (T.add t
+                             (aux
+                                (inc (inc depth))
+                                (B (h, inc depth, T.empty))
+                                (t :: r))
+                             m)
+                      , d )
               else
                 match T.find_opt h m with
-                | Some i -> R (c, Some (T.add t (aux i (t :: r)) m), d)
+                | Some i ->
+                    R
+                      ( c
+                      , depth
+                      , Some (T.add t (aux (inc depth) i (t :: r)) m)
+                      , d )
                 | None ->
-                    R (c, Some (T.add t (aux (B (h, T.empty)) (t :: r)) m), d) )
+                    R
+                      ( c
+                      , depth
+                      , Some
+                          (T.add t
+                             (aux
+                                (inc (inc depth))
+                                (B (h, inc depth, T.empty))
+                                (t :: r))
+                             m)
+                      , d ) )
           | None ->
               if S.eq c h then
-                R (c, Some (T.add t (aux Nil (t :: r)) T.empty), d)
-              else R (h, Some (T.add t (aux Nil (t :: r)) T.empty), d) ) )
+                R
+                  ( c
+                  , depth
+                  , Some (T.add t (aux (inc depth) Nil (t :: r)) T.empty)
+                  , d )
+              else
+                R
+                  ( h
+                  , depth
+                  , Some (T.add t (aux (inc depth) Nil (t :: r)) T.empty)
+                  , d ) ) )
     in
-    aux m char_list
+    aux 0 m char_list
 end
 
 module Set = struct
@@ -162,9 +201,14 @@ let () =
   *)
   A.get (explode "doofus") baz |> Printf.printf "%d\n" ;
   A.get (explode "doodoo") baz |> Printf.printf "%d\n" ;
+  (*
   A.get (explode "howdydoodymotherfuckerthisoneislooong") baz
   |> Printf.printf "%d\n" ;
   A.get (explode "howdydoodymotherfuckerthatoneisloooong") baz
   |> Printf.printf "%d\n" ;
   try A.get (explode "doo") baz |> Printf.printf "%d\n"
+
   with Not_found -> print_string "expected results\n"
+*)
+
+  A.get_fail_path 'd' baz |> A.display
