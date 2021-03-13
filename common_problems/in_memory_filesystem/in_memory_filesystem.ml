1adb0fb2b5aea345814516e8eae552cfc948f83f
matt@pallissard.net <matt@pallissard.net>
2021-03-12 19:20:45 -0800

    in memory fs

--- common_problems/in_memory_filesystem/in_memory_filesystem.ml
+++ common_problems/in_memory_filesystem/in_memory_filesystem.ml
@@ -1,9 +1,25 @@
 type content = S of string | Int of int | Bytes of bytes | Dir
 
+type key = string
+
 type errors = Exists_file
 
-module FS = struct
+module type FS = sig
+  type 'a m
+
+  val get : key -> content m -> content
+
+  val empty : 'a m
+
+  val mkdir : content m -> key -> (content m, errors) result
+
+  val write : content m -> key -> content -> (content m, errors) result
+end
+
+module Fs : FS = struct
   module Tf = Map.Make (String)
+
+  type 'a m = 'a Tf.t
 
   let empty = Tf.empty
 
@@ -17,11 +33,10 @@ module FS = struct
 
   let get path = Tf.find path
 
-
   let write tree path contents =
     match stat path tree with
     | true -> Error Exists_file
-    | false -> (
+    | false ->
         let rec aux tree root = function
           | [] -> Ok (set path contents tree)
           | h :: t -> (
@@ -31,7 +46,7 @@ module FS = struct
               | false -> aux (set path Dir tree) path t )
         in
         aux tree "" (splitpath path)
-    )
+
   let mkdir tree path = write tree path Dir
 end
 
@@ -40,35 +55,41 @@ let display i j k = Printf.printf i j k
 exception Can't_display_type
 
 let () =
-
   let rec aux tree = function
     | [] -> ()
-    | h :: t -> try match FS.get h tree with
-      | S i -> display "%s: %s\n" h i; aux tree t
-      | Int i -> display "%s: %d\n" h i; aux tree t
-      | Dir -> display "%s: %s\n" h "dir"; aux tree t
-      | _ -> raise Can't_display_type
-    with Not_found -> Printf.printf "notfound: %s\n" h
+    | h :: t -> (
+      try
+        match Fs.get h tree with
+        | S i -> display "%s: %s\n" h i ; aux tree t
+        | Int i -> display "%s: %d\n" h i ; aux tree t
+        | Dir -> display "%s: %s\n" h "dir" ; aux tree t
+        | _ -> raise Can't_display_type
+      with Not_found -> Printf.printf "notfound: %s\n" h )
   in
+  let b =
+    match Fs.mkdir Fs.empty "/foo/bar/baz" with
+    | Ok i -> i
+    | Error i -> (
+      match i with
+      | Exists_file ->
+          Printf.printf "%s\n" "exists file" ;
+          raise Can't_display_type )
+  in
+  aux b ["/foo"; "/foo/bar"; "/foo/bar/baz"] ;
+  Printf.printf "%s" "fuck\n" ;
+  let a =
+    match Fs.write b "/foo/bar/baz" (Int 1337) with
+    | Ok i -> i
+    | Error i -> (
+      match i with
+      | Exists_file ->
+          Printf.printf "exists :%s\n" "/foo/bar/baz" ;
+          raise Can't_display_type )
+  in
+  aux a ["/foo"; "/foo/bar"; "/foo/bar/baz"]
 
-  let b = match FS.mkdir FS.empty "/foo/bar/baz" with
-   | Ok i -> i
-   | Error i -> match i with
-    | Exists_file -> Printf.printf "%s\n" "exists file";
-     raise Can't_display_type
-  in
-  aux b ["/foo";"/foo/bar";"/foo/bar/baz"];
-  Printf.printf "%s" "fuck\n";
-  let a = match FS.write b "/foo/bar/baz" (Int 1337) with
-   | Ok i -> i
-   | Error i -> match i with
-    | Exists_file -> Printf.printf "exists :%s\n" "/foo/bar/baz";
-     raise Can't_display_type
-  in
-
-  aux a ["/foo";"/foo/bar";"/foo/bar/baz"]
-  (*
-  match FS.set "foo" (S "stringfile") FS.empty |> FS.get "foo" with
+(*
+  match Fs.set "foo" (S "stringfile") Fs.empty |> Fs.get "foo" with
   | S i -> display "%s" i
   | Int i -> display "%d" i
   | Dir | Bytes _ -> raise Can't_display_type
